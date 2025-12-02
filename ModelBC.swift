26178490c3511a85d227fa9688d3c7150c89c41a
Krrishnanunni <krishnanunni2004ka@gmail.com>
2025-12-02 14:43:01 +0530

    Data Model

---
+++ ModelBC.swift
@@ -0,0 +1,18 @@
+//
+//  ModelBC.swift
+//  SwiftfulThinkingSwiftUI
+//
+//  Created by Krishnanunni K A on 02/12/25.
+//
+
+import SwiftUI
+
+struct ModelBC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModelBC()
+}
