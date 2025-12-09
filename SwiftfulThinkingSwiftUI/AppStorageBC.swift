bf0aa686480faffee89adefc7b5f026f6a66797f
Krrishnanunni <krishnanunni2004ka@gmail.com>
2025-12-09 22:03:21 +0530

    AppStorage (UD) Storing Using UserDefaults

---
+++ SwiftfulThinkingSwiftUI/AppStorageBC.swift
@@ -0,0 +1,39 @@
+//
+//  AppStorageBC.swift
+//  SwiftfulThinkingSwiftUI
+//
+//  Created by Krishnanunni K A on 09/12/25.
+//
+
+import SwiftUI
+
+struct AppStorageBC: View {
+    @State var currentUsername: String?
+    var body: some View {
+        VStack{
+            if let name = currentUsername {
+                Text(name)
+            }
+            
+            Button("Save"){
+                let name:String = "kichu"
+                currentUsername = name
+//                UserDefaults.standard.set(name, forKey: "name")
+                
+            }
+            .padding()
+            .foregroundStyle(.white)
+            .background(Color.blue)
+            .cornerRadius(30)
+            
+        }
+        .onAppear{
+//            currentUsername = UserDefaults.standard.string(forKey: "name")
+            
+        }
+    }
+}
+
+#Preview {
+    AppStorageBC()
+}
