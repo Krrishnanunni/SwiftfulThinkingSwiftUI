090c84599ed5237b055393fd8819bbf492a41db9
Krrishnanunni <krishnanunni2004ka@gmail.com>
2025-12-10 23:00:04 +0530

    IntroView Created

---
+++ SwiftfulThinkingSwiftUI/OnBoardingViews/IntroView.swift
@@ -0,0 +1,34 @@
+//
+//  IntroView.swift
+//  SwiftfulThinkingSwiftUI
+//
+//  Created by Krishnanunni K A on 10/12/25.
+//
+
+import SwiftUI
+
+struct IntroView: View {
+    @AppStorage("is_signed") var currentUserSignedIn: Bool = false
+    var body: some View {
+        ZStack{
+            RadialGradient(colors: [Color.red , Color.purple ],
+                           center: .topLeading,
+                           startRadius: 5,
+                           endRadius: UIScreen.main.bounds.height)
+            .ignoresSafeArea()
+            
+            if currentUserSignedIn {
+                Text("Welcome User")
+                    .font(.title2)
+            }else{
+                Text("On Boarding Page")
+                    .font(.title2)
+            }
+                
+        }
+    }
+}
+
+#Preview {
+    IntroView()
+}
